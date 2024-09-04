b3f5a9373c5116705f21c35a859efdf16b8c7d5e
AxxellB <axxellblaze@gmail.com>
2024-09-04 17:55:04 +0300

    Homepage improvements

--- W3E6/index.php
+++ W3E6/index.php
@@ -2,5 +2,19 @@
 include 'header.php'
 ?>
 
-<h1>Home</h1>
+<div class="home-page-image"><img src="static/img/homepage.jpg"></div>
+<h2 class="container">Best courses</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card text-center" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
